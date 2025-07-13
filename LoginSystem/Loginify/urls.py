3aacbabe9a6fe4114f0858469ff7d458d6cb16ec
amansingh202 <kumaraman08071997@gmail.com>
2025-07-13 17:20:51 -0500

    urls updated for each CRUD methods

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(' ', views.hello_world, name="hello"),
+    path('', views.hello_world, name="hello"),
     path('signup/', views.signup, name = 'signup'),
-    path('login', views.login_view, name = 'login'),
-    path('get_all_users/', views.get_all_users, name = 'get_all_users'),
-    path('get_user_by_email/', views.get_user_by_email, name = 'get_user_by_email'),
-    path('update_user/', views.update_user, name = 'update_user'),
-    path('delete_user/', views.delete_user, name = 'delete_user')
+    path('login/', views.login_view, name = 'login'),
+
+
+    ## CRUD operations
+    path('get-all-users/', views.get_all_users, name='get_all_users'),
+    path('get-user/', views.get_user_by_email, name='get_user_by_email'),
+    path('update-user/', views.update_user, name='update_user'),
+    path('delete-user/', views.delete_user, name='delete_user'),
 ]
