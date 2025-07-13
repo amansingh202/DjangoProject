1534fe010b5c872c0b4747d0bbbcf26ab028db62
amansingh202 <kumaraman08071997@gmail.com>
2025-07-13 16:23:09 -0500

    crud operations in views added to urls.py

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path(' ', views.hello_world, name="hello"),
     path('signup/', views.signup, name = 'signup'),
-    path('login/', views.login, name = 'login')
+    path('login', views.login_view, name = 'login'),
+    path('get_all_users/', views.get_all_users, name = 'get_all_users'),
+    path('get_user_by_email/', views.get_user_by_email, name = 'get_user_by_email'),
+    path('update_user/', views.update_user, name = 'update_user'),
+    path('delete_user/', views.delete_user, name = 'delete_user')
 ]
